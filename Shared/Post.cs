668b9e0c1af0270bf7beea6d6353058aa2fe4662
--unset <bpriyan18082004@gmail.com>
2025-06-06 16:27:00 +0530

    added styles

--- Shared/Post.cs
+++ Shared/Post.cs
@@ -3,6 +3,7 @@ public class Post
     public string? Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
     public DateTime CreatedAt { get; set; }
     public string UserId { get; set; } = string.Empty;
     public string AuthorName { get; set; } = string.Empty;
